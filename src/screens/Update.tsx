57b820571b738715ffe6b9b656df9808b1628326
OrianSm <surajmujawar2002@gmail.com>
2024-12-16 15:43:24 +0530

    UpdateScreen

--- src/screens/Update.tsx
+++ src/screens/Update.tsx
@@ -82,7 +82,7 @@ const EditTherapy: React.FC<EditTherapyProps> = ({
   const [hasLiveSwitchAccess, setHasLiveSwitchAccess] =
     useState<boolean>(false);
 
-  const appointmentTypes = ["LiveSwitch", "In Clinic", "In Home", "IP/ICU"];
+  const appointmentTypes = ["Liveswitch", "In Clinic", "In Home", "IP/ICU"];
   useEffect(() => {
     fetchDoctors();
   }, []);
